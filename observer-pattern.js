e5fe6787b4845825b8b024f2376ab6e250d1fe69
Daisy1213 <daisyzs1213@163.com>
2018-05-01 22:06:03 +0800

    method rename

--- observer-pattern.js
+++ observer-pattern.js
@@ -1,17 +1,17 @@
 const Observer = require('./observer') ;
 const Subject = require('./subject') ;
 
-let shasha_do_sth = () => console.log('shasha恭喜你');
-let xiaoxin_do_sth = () => console.log('xiaoxin恭喜你');
-let xiaohong_do_sth = () => console.log('xiaohong恭喜你');
+let shasha_for_marry_do_sth = () => console.log('shasha恭喜你');
+let xiaoxin_for_marry_do_sth = () => console.log('xiaoxin恭喜你');
+let xiaohong_for_marry_do_sth = () => console.log('xiaohong恭喜你');
 
 let wanghao = new Subject();
 
-wanghao.register("marry", new Observer(shasha_do_sth));
-wanghao.register("marry", new Observer(xiaoxin_do_sth));
+wanghao.register("marry", new Observer(shasha_for_marry_do_sth));
+wanghao.register("marry", new Observer(xiaoxin_for_marry_do_sth));
 wanghao.publishEvent("marry");
 
-wanghao.register("marry", new Observer(xiaohong_do_sth));
+wanghao.register("marry", new Observer(xiaohong_for_marry_do_sth));
 wanghao.publishEvent("marry");
 
 
