992b4090cb765001ba19a40bef3f617e72defd4c
Daisy1213 <daisyzs1213@163.com>
2018-05-01 22:04:59 +0800

    refactor to one line

--- observer-pattern.js
+++ observer-pattern.js
@@ -1,18 +1,9 @@
 const Observer = require('./observer') ;
 const Subject = require('./subject') ;
 
-let shasha_do_sth = () => {
-    console.log('shasha恭喜你');
-}
-
-let xiaoxin_do_sth = () => {
-    console.log('xiaoxin恭喜你');
-}
-
-let xiaohong_do_sth = () => {
-    console.log('xiaohong恭喜你');
-}
-
+let shasha_do_sth = () => console.log('shasha恭喜你');
+let xiaoxin_do_sth = () => console.log('xiaoxin恭喜你');
+let xiaohong_do_sth = () => console.log('xiaohong恭喜你');
 
 let wanghao = new Subject();
 
