f5e7f91a9f3a41091c5c8650977edb758d0a30a0
Daisy1213 <daisyzs1213@163.com>
2018-05-01 22:14:29 +0800

    adjust statement order

--- observer-pattern.js
+++ observer-pattern.js
@@ -1,13 +1,15 @@
 const Observer = require('./observer') ;
 const Subject = require('./subject') ;
 
+let wanghao = new Subject();
+
 let shasha_for_marry_do_sth = () => console.log('shasha恭喜你');
 let xiaoxin_for_marry_do_sth = () => console.log('xiaoxin恭喜你');
 let xiaohong_for_marry_do_sth = () => console.log('xiaohong恭喜你');
 
-let wanghao = new Subject();
-
 wanghao.register("marry", new Observer(shasha_for_marry_do_sth));
+wanghao.publishEvent("marry");
+
 wanghao.register("marry", new Observer(xiaoxin_for_marry_do_sth));
 wanghao.publishEvent("marry");
 
@@ -16,14 +18,14 @@ wanghao.publishEvent("marry");
 
 
 let shasha_for_have_first_child = () => console.log("喜得贵子_from_shasha");
+let xiaoxin_for_have_second_child = () => console.log("喜得贵子_from_xiaoxin");
+let xiaohong_for_have_second_child = () => console.log("喜得贵子_from_xiaohong");
 
 wanghao.register("have_child", new Observer(shasha_for_have_first_child));
 wanghao.publishEvent("have_child");
 
-let xiaoxin_for_have_second_child = () => console.log("喜得贵子_from_xiaoxin");
 wanghao.register("have_child", new Observer(xiaoxin_for_have_second_child));
 wanghao.publishEvent("have_child");
 
-let xiaohong_for_have_second_child = () => console.log("喜得贵子_from_xiaohong");
 wanghao.register("have_child", new Observer(xiaohong_for_have_second_child));
 wanghao.publishEvent("have_child");
