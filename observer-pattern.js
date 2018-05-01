00312d4a0e8eb52a38c8af333fc06bdcc7a3a90b
Daisy1213 <cfachen@thoughtworks.com>
2018-05-01 21:16:56 +0800

    method rename

--- observer-pattern.js
+++ observer-pattern.js
@@ -1,28 +1,28 @@
 const Observer = require('./observer') ;
 const Subject = require('./subject') ;
 
-let shasha_dosth = () => {
+let shasha_do_sth = () => {
     console.log('shasha恭喜你');
 }
 
-let xiaoxin_dosth = () => {
+let xiaoxin_do_sth = () => {
     console.log('xiaoxin恭喜你');
 }
 
-let xiaohong_dosth = () => {
+let xiaohong_do_sth = () => {
     console.log('xiaohong恭喜你');
 }
 
 
-let shasha = new Observer(shasha_dosth);
-let xiaoxin = new Observer(xiaoxin_dosth);
+let shasha = new Observer(shasha_do_sth);
+let xiaoxin = new Observer(xiaoxin_do_sth);
 let wanghao = new Subject();
 
 wanghao.register(shasha);
 wanghao.register(xiaoxin);
 wanghao.marrige();
 
-let xiaohong = new Observer(xiaohong_dosth);
+let xiaohong = new Observer(xiaohong_do_sth);
 wanghao.register(xiaohong);
 wanghao.marrige();
 
